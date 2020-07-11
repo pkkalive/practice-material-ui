6ef221c01803a527c2ad4e5840f945bbb1d11eb5
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2020-07-11 19:08:51 +0530

    added links to tabs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { ThemeProvider } from "@material-ui/styles";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import theme from './ui/Theme';
 import Header from "./ui/Header";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Header />
-      Hello World
+        <BrowserRouter>
+            <Header />
+            <Switch>
+                <Route exact path ="/" component= {() => <div>HOME</div>}/>
+                <Route exact path ="/services" component= {() => <div>SERVICES</div>}/>
+                <Route exact path ="/revolution" component= {() => <div>THE REVOLUTION</div>}/>
+                <Route exact path ="/about" component= {() => <div>ABOUT US</div>}/>
+                <Route exact path ="/contact" component= {() => <div>CONTACT US</div>}/>
+                <Route exact path ="/profile" component= {() => <div>MY PROFILE</div>}/>
+                <Route exact path ="/logout" component= {() => <div>LOGOUT</div>}/>
+            </Switch>
+        </BrowserRouter>
     </ThemeProvider>
   );
 }
--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import {Home, Settings, History, Info, PermContactCalendar, ExpandMore } from '@material-ui/icons';
-
-import logo from '../../assets/logo.svg';
-import NativeSelect from "@material-ui/core/NativeSelect";
+import {Home, Settings, History, Info,
+    PermContactCalendar, ExpandMore } from '@material-ui/icons';
 import ButtonGroup from "@material-ui/core/ButtonGroup";
 import Button from "@material-ui/core/Button";
 import Avatar from "@material-ui/core/Avatar";
+import Popper from "@material-ui/core/Popper";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -46,39 +49,72 @@ const useStyles = makeStyles(theme => ({
     button:{
         border: "none",
         boxShadow: "none",
+        marginRight: "25px",
+        marginLeft: "50px"
     },
     avatar: {
         marginLeft: "5px"
+    },
+    popper: {
+        left: "15px !important"
     }
+
 }));
 
 export default function Header(props) {
     const classes = useStyles();
-    const [value, setValue] = React.useState(0);
+    const [value, setValue] = useState(0);
+    const [open, setOpen] = useState(false);
+    const anchorRef = useRef(null);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
+    };
+
+    const handleClose = (event) => {
+        if (anchorRef.current) {
+            return;
+        }
+        setOpen(false);
+    };
+
     return(
         <React.Fragment>
             <ElevationScroll>
                 <AppBar position="fixed" color="primary">
                     <Toolbar disableGutters>
                         <img src={logo} alt="Company Logo" className={classes.logo}/>
-                        <Tabs className={classes.tabContainer} value={value} onChange={handleChange} >
-                            <Tab className={classes.tab} icon={<Home />} label="HOME" />
-                            <Tab className={classes.tab} icon={<Settings />} label="SERVICES" />
-                            <Tab className={classes.tab} icon={<History />} label="THE REVOLUTION" />
-                            <Tab className={classes.tab} icon={<Info />} label="ABOUT US" />
-                            <Tab className={classes.tab} icon={<PermContactCalendar />} label="CONTACT US" />
+                        <Tabs className={classes.tabContainer} value={value} onChange={handleChange} indicatorColor="primary">
+                            <Tab className={classes.tab} icon={<Home />} label="HOME" component={Link} to="/"/>
+                            <Tab className={classes.tab} icon={<Settings />} label="SERVICES" component={Link} to="/services"/>
+                            <Tab className={classes.tab} icon={<History />} label="THE REVOLUTION" component={Link} to="/revolution"/>
+                            <Tab className={classes.tab} icon={<Info />} label="ABOUT US" component={Link} to="/about"/>
+                            <Tab className={classes.tab} icon={<PermContactCalendar />} label="CONTACT US" component={Link} to="/contact"/>
                         </Tabs>
-                        <ButtonGroup className={classes.button} variant="contained" color="primary">
-                            <Button>
+                        <ButtonGroup className={classes.button} variant="contained" color="primary" ref={anchorRef}>
+                            <Button onClick={handleToggle}
+                                    aria-label="select merge strategy">
                                 Hi Kumar
                                 <Avatar className={classes.avatar}></Avatar>
                                 <ExpandMore />
                             </Button>
                         </ButtonGroup>
+                        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal className={classes.popper}>
+                            {({ TransitionProps, placement }) => (
+                                <Paper >
+                                    <ClickAwayListener onClickAway={handleClose}>
+                                        <MenuList id="split-button-menu">
+                                            <MenuItem component={Link} to="/profile" >My Profile</MenuItem>
+                                            <MenuItem component={Link} to="/logout" >Logout</MenuItem>
+                                        </MenuList>
+                                    </ClickAwayListener>
+                                </Paper>
+                            )}
+                        </Popper>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
