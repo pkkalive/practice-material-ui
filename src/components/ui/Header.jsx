771371295d630223dca870fcbf93f3253ba4d06e
Purushotham Kumar <kalivepurushothamkumar@gmail.com>
2020-07-11 17:14:01 +0530

    Added icon tabs

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -4,6 +4,10 @@ import Toolbar from "@material-ui/core/Toolbar";
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import {Home, Settings, History, Info, PermContactCalendar } from '@material-ui/icons';
 
 import logo from '../../assets/logo.svg';
 
@@ -24,19 +28,39 @@ const useStyles = makeStyles(theme => ({
         ...theme.mixins.toolbar,
         marginBottom: "3em"
     },
-    logo:{
+    logo: {
         height: "7em"
+    },
+    tabContainer: {
+        marginLeft: "auto"
+    },
+    tab: {
+        ...theme.typography.tab,
+        minWidth: 10,
+        marginLeft: "25px"
     }
 }));
 
 export default function Header(props) {
     const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return(
         <React.Fragment>
             <ElevationScroll>
                 <AppBar position="fixed" color="primary">
                     <Toolbar disableGutters>
                         <img src={logo} alt="Company Logo" className={classes.logo}/>
+                        <Tabs className={classes.tabContainer} value={value} onChange={handleChange} >
+                            <Tab className={classes.tab} icon={<Home />} label="HOME" />
+                            <Tab className={classes.tab} icon={<Settings />} label="SERVICES" />
+                            <Tab className={classes.tab} icon={<History />} label="THE REVOLUTION" />
+                            <Tab className={classes.tab} icon={<Info />} label="ABOUT US" />
+                            <Tab className={classes.tab} icon={<PermContactCalendar />} label="CONTACT US" />
+                        </Tabs>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
